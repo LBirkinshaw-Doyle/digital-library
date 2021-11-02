021ce55d9df805cbd925621b47905260290ef0e8
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2021-11-02 13:59:13 +0000

    adding title, add books button, and the base formatting

--- scripts/active.js
+++ scripts/active.js
@@ -3,6 +3,10 @@ const githubLogo = document.querySelector("#github-logo");
 githubLink.addEventListener("pointerenter", () => githubLogo.style.transform = "rotate(540deg)");
 githubLink.addEventListener("pointerleave", () => githubLogo.style.transform = "rotate(0deg)");
 
+const bookcase = document.querySelector("#bookcase");
+const toolbar = document.querySelector("#toolbar");
+const addBooksButton = document.querySelector("#add-books");
+
 const book = {
     init: function (title, authorLast, authorOther, pages) {
         this.title = title;
@@ -14,3 +18,6 @@ const book = {
     type: "book",
     toggleRead: function() {this.read? this.read = false : this.read = true;}, 
 }
+
+let bookArray = [];
+
