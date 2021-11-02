99bb08c6f335d97c2745bddfbfa5c9d4c93cc30d
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2021-11-02 11:39:19 +0000

    added book prototype object

--- scripts/active.js
+++ scripts/active.js
@@ -2,3 +2,15 @@ const githubLink = document.querySelector("#github-link");
 const githubLogo = document.querySelector("#github-logo");
 githubLink.addEventListener("pointerenter", () => githubLogo.style.transform = "rotate(540deg)");
 githubLink.addEventListener("pointerleave", () => githubLogo.style.transform = "rotate(0deg)");
+
+const book = {
+    init: function (title, authorLast, authorOther, pages) {
+        this.title = title;
+        this.authorLastName = authorLast;
+        this.authorOtherName = authorOther;
+        this.pages = pages;
+        this.read = false;
+    },
+    type: "book",
+    toggleRead: function() {this.read? this.read = false : this.read = true;}, 
+}
